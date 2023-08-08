92bd082e7ffd8de4f5e9530a424551c3a05fe657
dasbiplob <biplob90.d@git.com>
2023-08-08 14:43:07 +0300

    Modifed the databaselink

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://127.0.0.1:27017/meanauth',
+    //database: 'mongodb://127.0.0.1:27017/meanauth',
+    database: 'mongodb+srv://biplob90d:Finland@2023@meanauth.vh2msth.mongodb.net/?retryWrites=true&w=majority',
     secret: 'yoursecret'
 }
