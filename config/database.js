aeb07b1f6b3d80d0c843086b2b9d55a8120500fa
dasbiplob <biplob90.d@git.com>
2023-08-08 15:03:43 +0300

    Added the comment on databse.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
     //database: 'mongodb://127.0.0.1:27017/meanauth',
+    //DataBase MongoDB connection from Cloud
     database: 'mongodb+srv://biplob90d:Finland@2023@meanauth.vh2msth.mongodb.net/?retryWrites=true&w=majority',
     secret: 'yoursecret'
 }
