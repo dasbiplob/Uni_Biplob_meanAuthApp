9238b1f892a86692c872616338f12572ee44c094
dasbiplob <biplob90.d@git.com>
2023-08-14 11:54:38 +0300

    Modifed the Profile Page

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -12,14 +12,14 @@ export class AuthService {
   user: any;
 
   //For Production
-  /* private _registerUrl = "users/register"
+  private _registerUrl = "users/register"
   private _authenticateUrl = "users/authentication"
-  private _usersProfile = "users/profile" */
+  private _usersProfile = "users/profile"
 
   // For Dev
-  private _registerUrl = "http://localhost:3000/users/register"
+  /* private _registerUrl = "http://localhost:3000/users/register"
   private _authenticateUrl = "http://localhost:3000/users/authentication"
-  private _usersProfile = "http://localhost:3000/users/profile"
+  private _usersProfile = "http://localhost:3000/users/profile" */
 
   constructor(private http:HttpClient) {}
 
--- app.js
+++ app.js
@@ -28,8 +28,10 @@ mongoose.connect(config.database,{
 
 
 const app = express();
-//const port = process.env.PORT || 8080;
-const port = process.env.PORT || 3000;
+//For Prod
+const port = process.env.PORT || 8080;
+//For Dev
+//const port = process.env.PORT || 3000;
 
 const users = require('./routes/users.js'); 
 
